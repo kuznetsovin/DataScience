4d9b417753fe40315cb118e69d2d7f1143d171dd
Igor <igorkuznetsov@MacBook-Pro-Igor.local>
2013-10-22 22:29:59 +0400

    Тест вывода

--- VisualisationMapPython/main.py
+++ VisualisationMapPython/main.py
@@ -5,12 +5,11 @@ Created on Tue Oct 22 11:43:08 2013
 @author: IgKuznetsov
 """
 import vincent
-vincent.core.initialize_notebook()
 
-world_topo = r'world-countries.topo.json'
+world_topo = r'TestWorldMap/world-countries.topo.json'
 geo_data = [{'name': 'countries',
              'url': world_topo,
              'feature': 'world-countries'}]
 
 vis = vincent.Map(geo_data=geo_data, scale=200)
-vis.display()
+vis.to_json('map.json', html_out=True, html_path='map.html')
