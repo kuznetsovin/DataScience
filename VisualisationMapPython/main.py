fc877a7a6193f1acb3575f35987f25289f0a14a0
Igor <igorkuznetsov@MacBook-Pro-Igor.local>
2013-10-23 20:18:31 +0400

    парсинг страницы с росстата

--- VisualisationMapPython/main.py
+++ VisualisationMapPython/main.py
@@ -14,18 +14,21 @@ Created on Tue Oct 22 11:43:08 2013
 #vis.to_json('example_map.json', html_out=True, html_path='example_map.html')
 
 import pandas as pd
-import requests
+#import requests
 
 
-url = u'http://ru.wikipedia.org/wiki/Список_городов_России'
-City = pd.read_html(
-    requests.get(url,headers={'User-agent': 'Mozilla/5.0'}).text, 
-    header=0, 
-    attrs = {'class':'wikitable sortable'})[0]
-CityRegion = City[[u'Регион',u'Город']]
-CityRegion.rename(columns={u'Регион':'region',u'Город':'city_name'}, inplace=True)
-CityRegion.region = CityRegion.region.str.replace(u'авт. окр.',u'автономный округ')
-spr = pd.read_csv('RusMap/russia-region-names.tsv','\t',header=None, names = ['region','code'], encoding='utf-8')
-spr.region = spr.region.str.replace(u'Республика ','')
-spr.region = spr.region.str.replace(u'Город ','')
-a = CityRegion.merge(spr, 'left', 'region')
+#url = u'http://ru.wikipedia.org/wiki/Список_городов_России'
+#City = pd.read_html(
+#    requests.get(url,headers={'User-agent': 'Mozilla/5.0'}).text, 
+#    header=0, 
+#    attrs = {'class':'wikitable sortable'})[0]
+#CityRegion = City[[u'Регион',u'Город']]
+#CityRegion.rename(columns={u'Регион':'region',u'Город':'city_name'}, inplace=True)
+#CityRegion.region = CityRegion.region.str.replace(u'авт. окр.',u'автономный округ')
+#spr = pd.read_csv('RusMap/russia-region-names.tsv','\t',header=None, names = ['region','code'], encoding='utf-8')
+#spr.region = spr.region.str.replace(u'Республика ','')
+#spr.region = spr.region.str.replace(u'Город ','')
+#a = CityRegion.merge(spr, 'left', 'region')
+url = u'http://www.gks.ru/bgd/regl/b12_14p/IssWWW.exe/Stg/d01/05-02.htm'
+City = pd.read_html(url)[0]
+
