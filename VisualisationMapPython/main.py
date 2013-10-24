bd91dd12bc49f57e01784794d74c6ef9cf9c43cd
kuznetsovin <kuznetsovin@gmail.com>
2013-10-24 12:35:39 +0400

    Предфинальная версия скрипта

--- VisualisationMapPython/main.py
+++ VisualisationMapPython/main.py
@@ -4,31 +4,35 @@ Created on Tue Oct 22 11:43:08 2013
 
 @author: IgKuznetsov
 """
-#import vincent
-#rus = 'RusMap/russia.json'
-#geo_data = [{'name': 'states',
-#             'url': rus,
-#             'feature': 'russia'}]
-#
-#vis = vincent.Map(geo_data=geo_data, scale=700, projection='conicEqualArea', rotate = [-105,0], center = [-10, 65])
-#vis.to_json('example_map.json', html_out=True, html_path='example_map.html')
 
 import pandas as pd
-#import requests
 
+#загружаем статистику регионов и справочник с кодами регионов используемыми в карте
+stat = pd.read_html('Data/AVGPeopleProfit.htm', header=0, index_col=0)[0]
+spr = pd.read_csv('Data/russia-region-names.tsv','\t', index_col=0, header=None, names = ['name','code'], encoding='utf-8')
+#приводим имена ригеонов в стат данных к справочному виду и обновляем индекс
+new_index = stat.index.to_series().str.replace(u'(2\))|(1\))|(г. )','')
+stat.set_index(new_index, inplace=True)
+#добавляем к стат данным коды регионов для построения карты
+RegionProfit = stat.join(spr, how='inner')
+#удяляем лишние неинформаивные столбцы
+RegionProfit.drop([1990], axis=1)
 
-#url = u'http://ru.wikipedia.org/wiki/Список_городов_России'
-#City = pd.read_html(
-#    requests.get(url,headers={'User-agent': 'Mozilla/5.0'}).text, 
-#    header=0, 
-#    attrs = {'class':'wikitable sortable'})[0]
-#CityRegion = City[[u'Регион',u'Город']]
-#CityRegion.rename(columns={u'Регион':'region',u'Город':'city_name'}, inplace=True)
-#CityRegion.region = CityRegion.region.str.replace(u'авт. окр.',u'автономный округ')
-#spr = pd.read_csv('RusMap/russia-region-names.tsv','\t',header=None, names = ['region','code'], encoding='utf-8')
-#spr.region = spr.region.str.replace(u'Республика ','')
-#spr.region = spr.region.str.replace(u'Город ','')
-#a = CityRegion.merge(spr, 'left', 'region')
-url = u'http://www.gks.ru/bgd/regl/b12_14p/IssWWW.exe/Stg/d01/05-02.htm'
-City = pd.read_html(url)[0]
-
+import vincent
+#подключаем карту d3
+geo_data = [{'name': 'rus',
+             'url': 'RusMap/russia.json',
+             'feature': 'russia'}]
+#отрисовываем карту с привязкой стат данных по регионмам
+vis = vincent.Map(data=RegionProfit, geo_data=geo_data,scale=700, projection='conicEqualArea', rotate = [-105,0], center = [-10, 65],
+          data_bind=2011, data_key='code',
+          map_key={'rus': 'properties.region'})
+#задаем полупрозрачные границы регионов
+vis.marks[0].properties.enter.stroke_opacity = vincent.ValueRef(value=0.5)
+#задаем градацию по цвету для группы для отображения на карте
+vis.scales['color'].type = 'threshold'
+vis.scales['color'].domain = [0, 10000, 15000, 20000, 25000, 30000]
+#вводим название легенды карты
+vis.legend(title=u'Доходы руб.')
+#выгружаем итоговую карту
+vis.to_json('example_map.json', html_out=True, html_path='example_map.html')
