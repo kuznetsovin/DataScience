e9291786c692da421d1128e440f42a586489c051
Igor <igorkuznetsov@MacBook-Pro-Igor.local>
2013-10-31 20:09:03 +0400

    решение

--- Kaggle_DigitRec/answer.py
+++ Kaggle_DigitRec/answer.py
@@ -9,16 +9,23 @@ from pandas import read_csv
 from sklearn import cross_validation
 from sklearn.externals import joblib
 from sklearn.linear_model import LogisticRegression
+from sklearn import svm
 
-csv = read_csv('train.csv', header=None, skiprows=1)
+csv = read_csv('Data/train.csv')
 joblib.dump(csv, 'digit.pkl')
-dataset = joblib.load('digit.pkl')
 
 
 dataset = joblib.load('digit.pkl') #сюда были свалены данные, полученные после препроцессинга
 
-target = [x[0] for x in dataset]
-train = [x[1:] for x in dataset]
+
+target = dataset['label']
+train = dataset.drop('label',axis=1)
 
 glm = LogisticRegression(penalty='l1', tol=1)
+clf = svm.SVC()
+
 scores = cross_validation.cross_val_score(glm, train, target, cv = 10)
+scores1 = cross_validation.cross_val_score(clf, train, target, cv = 10)
+
+print scores
+print scores1
